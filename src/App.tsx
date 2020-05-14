74479c43a9062ebcdc4732fba61d6931f5bc3a04
raulpadilladelgado <rpd27@outlook.com>
2020-05-14 23:36:24 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Greetings from './components/greetings/greetings';
+
+function App() {
+  return (
+    <div className="App">
+      <Greetings/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/greetings/greetings.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const Greetings = () => {
+    return (
+        <h1>Hello</h1>
+    )
+};
+
+export default Greetings;
